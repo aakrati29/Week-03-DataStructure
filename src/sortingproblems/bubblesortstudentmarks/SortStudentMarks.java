737a5bb314540d0707474744c53bd9b80c27d791
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-01-31 15:54:48 +0530

    Bubble Sort Done

---
+++ src/sortingproblems/bubblesortstudentmarks/SortStudentMarks.java
@@ -0,0 +1,23 @@
+package sortingproblems.bubblesortstudentmarks;
+
+public class SortStudentMarks {
+
+    public static void sortStudentMarks(int arr[]){
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = 1; j < arr.length-i-1; i++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < arr.length-1; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {20,35,41,10,20};
+        sortStudentMarks(arr);
+    }
+}
